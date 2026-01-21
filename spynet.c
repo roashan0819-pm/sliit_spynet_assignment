5f0b3513dd881666665ec4ad9fc313c149a485cf
vboxuser <it25100943@my.sliit.lk>
2026-01-21 13:23:23 +0000

    Dynamic memory allocation for N x N grid created

--- spynet.c
+++ spynet.c
@@ -1,38 +1,47 @@
-#include<stdio.h>
-int main(){
-
-//@ player 1
-//& player 2
-//$ player 3
-//# Wall(bloked)
-//I intel
-//L lives 
-//X Extraction point
-//. Empty space 
-
-char s;
-
-printf("\n+---+---+---+---+---+");
-printf("\n| @ | . | I | # | . |");
-printf("\n+---+---+---+---+---+");
-printf("\n|-.-|-.-|-.-|-#-|-.-|");
-printf("\n+---+---+---+---+---+");
-printf("\n| L | L | # | . | I |");
-printf("\n+---+---+---+---+---+");
-printf("\n| . | # | . | I | . |");
-printf("\n+---+---+---+---+---+");
-printf("\n| . | L | X | . | . |");
-printf("\n+---+---+---+---+---+");
-	
 
 
+#include <stdio.h>
+#include <stdlib.h>
 
-
-
-
-
-
-
-return 0;
+void free_map(char **grid, int size) {
+for (int i = 0; i < size; i++) 
+    {	
+        free(grid[i]);
+    }
+    free(grid);
 }
 
+int main() {
+    int n;
+    printf("Enter grid size (5-15): ");
+    scanf("%d", &n);
+
+    if (n < 5 || n > 15) 
+	                                            // the limit is given in the assignment tute (5 to 15) size of grid
+    {  
+	printf("Invalid size. Defaulting to 5.\n");
+        n = 5;
+    }
+
+                                                        // dynamic Allocation 
+    char **grid = (char **)malloc(n * sizeof(char *));
+    for (int i = 0; i < n; i++)
+
+    {	    
+        grid[i] = (char *)malloc(n * sizeof(char));
+    }
+
+    
+    for (int i = 0; i < n; i++) {          //starting with the dots useing loops here.. and also used increment and rela                                             tional operators
+        for (int j = 0; j < n; j++) {
+            grid[i][j] = '.';
+            printf("%c ", grid[i][j]);
+        }
+        printf("\n");
+    }
+
+    
+
+    free_map(grid, n);
+    return 0;
+}
