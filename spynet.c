7ac5f5e8156b6d959d6294ac5d3c2d7ccdc7c4bb
vboxuser <it25100943@my.sliit.lk>
2026-01-21 11:42:16 +0000

    Maked demo grid

--- spynet.c
+++ spynet.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 int main(){
 
+//@ player 1
+//& player 2
+//$ player 3
+//# Wall(bloked)
+//I intel
+//L lives 
+//X Extraction point
+//. Empty space 
 
 char s;
 
@@ -8,6 +16,15 @@ printf("\n+---+---+---+---+---+");
 printf("\n| @ | . | I | # | . |");
 printf("\n+---+---+---+---+---+");
 printf("\n|-.-|-.-|-.-|-#-|-.-|");
+printf("\n+---+---+---+---+---+");
+printf("\n| L | L | # | . | I |");
+printf("\n+---+---+---+---+---+");
+printf("\n| . | # | . | I | . |");
+printf("\n+---+---+---+---+---+");
+printf("\n| . | L | X | . | . |");
+printf("\n+---+---+---+---+---+");
+	
+
 
 
 
